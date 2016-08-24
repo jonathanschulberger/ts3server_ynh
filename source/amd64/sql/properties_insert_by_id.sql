fd356a94eee0d09f23bc26949600e0e9d374e9fa
MidgetMob <jschoulzy@gmail.com>
2016-08-24 06:58:28 -0400

    [fix] Removed need for libmariadb2
    Removed uneeded source files
    
    Signed-off-by: MidgetMob <jschoulzy@gmail.com>

--- source/amd64/sql/properties_insert_by_id.sql
+++
@@ -1,4 +0,0 @@
-delete from :tablename: where id = :id: ;
-insert into :tablename: ( server_id, id, ident, value) 
-    select :server_id:, :id:, :ident*:, :value*:
-    [[[ union select :server_id:, :id:, :ident*:, :value*:]]]
--- source/x86/sql/properties_insert_by_string_id.sql
+++
@@ -1,4 +0,0 @@
-delete from :tablename: where string_id = :string_id: ;
-insert into :tablename: ( server_id, id, ident, value, string_id) 
-    select :server_id:, 0, :ident*:, :value*:, :string_id:
-    [[[union select :server_id:, 0, :ident*:, :value*:, :string_id:]]]
